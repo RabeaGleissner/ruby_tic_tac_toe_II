5a2a83e649703d3de4e3438345122f92da1a5111
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-13 16:02:23 +0000

    detects if there is a winner

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,37 @@ class Board
 
   def initialize(marks = [:E]*9)
     @grid = marks
+    @dimension = Math.sqrt(@grid.size).to_i
+  end
+
+  def winner
+    winner = false
+    lines.each do |line|
+      if all_x(line)
+        winner = :X
+      end
+
+      if all_o(line)
+        winner = :O
+      end
+    end
+    winner
+  end
+
+  def all_x(line)
+    all_x = true
+    line.each do |cell|
+      all_x = all_x && cell == :X
+    end
+    all_x
+  end
+
+  def all_o(line)
+    all_o = true
+    line.each do |cell|
+      all_o = all_o && cell == :O
+    end
+    all_o
   end
 
   def empty?
@@ -15,6 +46,49 @@ class Board
       end
     end
     true
+  end
+
+  def lines
+    [rows, columns, diagonals].flatten(1)
+  end
+
+  def rows
+    rows = []
+    @grid.each_slice(@dimension) do |row|
+      rows << row
+    end
+    rows
+  end
+
+  def columns
+    columns = []
+    index = 0
+    @dimension.times do
+      column = []
+      rows.each do |row|
+        column << row[index]
+      end
+      columns << column
+      index += 1
+    end
+    columns
+  end
+
+  def diagonals
+    first = []
+    index = 0
+    @dimension.times do
+      first << @grid[(@dimension * index) + index]
+      index +=1
+    end
+
+    second = []
+    offset = @dimension-1
+    @dimension.times do
+      second << @grid[offset]
+      offset += @dimension-1
+    end
+    [first, second]
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,7 @@
 require_relative 'spec_helper'
 require_relative '../lib/board'
 
+
 describe Board do
   let(:board) {Board.new}
 
@@ -9,9 +10,67 @@ describe Board do
   end
 
   it "is full when all cells are in use" do
-    full_board = Board.new([:X, :O, :X, :O, :X, :O, :X, :O, :X])
+    full_board = Board.new([:X, :O, :X,
+                            :O, :X, :O,
+                            :X, :O, :X])
     expect(full_board.full?).to be true
   end
 
+  it "has a winner X" do
+    winning_board = Board.new([:X, :X, :X,
+                               :O, :O, :E,
+                               :E, :E, :E])
+    expect(winning_board.winner).to eq(:X)
+  end
+
+  it "has a winner O" do
+    winning_board = Board.new([:E, :X, :X,
+                               :O, :O, :O,
+                               :E, :E, :E])
+    expect(winning_board.winner).to eq(:O)
+  end
+
+  it "has no winner" do
+    winning_board = Board.new([:E, :X, :X,
+                               :O, :X, :O,
+                               :E, :E, :E])
+    expect(winning_board.winner).to eq(false)
+  end
+
+  it "lists all rows" do
+    full_board = Board.new([:X, :O, :X,
+                            :O, :X, :O,
+                            :X, :O, :X])
+    expect(full_board.rows).to eq([[:X, :O, :X], [:O, :X, :O], [:X, :O, :X]])
+  end
+
+  it "lists all columns" do
+    board = Board.new([:X, :O, :E,
+                       :O, :X, :O,
+                       :E, :E, :E])
+    expect(board.columns).to eq([[:X, :O, :E], [:O, :X, :E], [:E, :O, :E]])
+  end
+
+  it "lists all diagonals" do
+    board = Board.new([:X, :O, :E,
+                       :O, :X, :O,
+                       :E, :E, :E])
+    expect(board.diagonals).to eq([[:X, :X, :E], [:E, :X, :E]])
+  end
+
+  it "lists all lines of the current game state" do
+    board = Board.new([:X, :O, :E,
+                       :O, :X, :O,
+                       :E, :E, :E])
+    expect(board.lines).to eq([[:X, :O, :E], [:O, :X, :O], [:E, :E, :E], [:X, :O, :E], [:O, :X, :E], [:E, :O, :E], [:X, :X, :E], [:E, :X, :E]])
+  end
+
+  it "knows that all all marks are x in line" do
+    expect(board.all_x([:X, :X, :X])).to be true
+  end
+
+  it "knows that all all marks are o in line" do
+    expect(board.all_o([:O, :O, :O])).to be true
+  end
 end
 
