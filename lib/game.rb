3395e7edbd887f235af20380a7bedd7062c3b0a8
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-14 18:06:13 +0000

    plays the game once

---
+++ lib/game.rb
@@ -0,0 +1,29 @@
+class Game
+  attr_reader :player1, :player2, :ui
+
+  def initialize(player1, player2, ui)
+    @player1 = player1
+    @player2 = player2
+    @ui = ui
+  end
+
+  def play
+    board = Board.new
+    current_player = player1
+    while !board.game_over?
+      board = current_player.make_move(board)
+      current_player = switch_player(current_player)
+    end
+    ui.announce_winner(board)
+
+  end
+
+  def switch_player(current_player)
+    if current_player == player1
+      player2
+    else
+      player1
+    end
+  end
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,43 @@
+require_relative 'spec_helper'
+require_relative '../lib/game.rb'
+
+describe Game do
+
+  it "plays game once" do
+    player1 = PlayerWithSetMoves.new(:X, [0,1,2])
+    player2 = PlayerWithSetMoves.new(:X, [3,4])
+    fake_ui = FakeUi.new
+    game = Game.new(player1, player2, fake_ui)
+    game.play
+    expect(fake_ui.announce_winner_called).to eq 1
+
+  end
+
+  class PlayerWithSetMoves
+    attr_reader :mark
+
+    def initialize(mark, moves)
+      @mark = mark
+      @moves = moves
+    end
+
+    def make_move(board)
+      board.add_mark(@moves.shift, mark)
+    end
+  end
+
+  class FakeUi
+    def initialize
+      @announce_winner_called = 0
+    end
+
+    def announce_winner(board)
+      @announce_winner_called += 1
+    end
+
+    def announce_winner_called
+      @announce_winner_called
+    end
+  end
+
+end
