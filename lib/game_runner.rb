4d1620b3fe8551d135f883e1e5c9ff0fd914e05a
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-21 17:11:36 +0000

    GameRunner uses player factory to create players

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -1,21 +1,22 @@
 require 'game'
+require 'player_factory'
+
 class GameRunner
-  attr_reader :ui, :player1, :player2
+  attr_reader :ui, :player_factory
 
   GAME_OPTIONS = {1 => "Human vs Human",
                   2 => "Human vs Computer",
                   3 => "Computer vs Human",
                   4 => "Quit program"}
 
-  def initialize(ui, player1, player2)
+  def initialize(ui, player_factory)
     @ui = ui
-    @player1 =  player1
-    @player2 = player2
+    @player_factory = player_factory
   end
 
   def set_up_game
     game_option = ui.menu(GAME_OPTIONS)
-    game = Game.new(player1, player2, ui)
+    game = Game.new(player_factory.create_player1(game_option), player_factory.create_player2(game_option), ui)
     game.play
     replay
   end
