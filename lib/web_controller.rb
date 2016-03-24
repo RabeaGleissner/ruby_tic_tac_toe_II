3a80bc770521d83f9459870fa17a0cdf0d6a8849
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-24 15:54:13 +0000

    displays the TTT board with current game state in the front end

--- lib/web_controller.rb
+++ lib/web_controller.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require 'haml'
+require 'web_ui'
+require 'game'
 
 class WebController < Sinatra::Base
   get '/' do
+    ui = WebUi.new
+    Game.new(ui)
+    @game_state_rows = ui.game_state_rows
     haml :index
   end
 end
